968a13c1e35b91b460e5cc8b38f40230081a6b63
qqiang2006 <344897531@qq.com>
2017-08-07 17:34:20 +0800

    icmp

--- icmp.py
+++ icmp.py
@@ -1,27 +1,33 @@
 #coding=utf8
-import struct,socket,time,random
+import struct,socket,time,random,os
 #校验和函数
-def do_checksum(data):
-    """  Verify the packet integritity """
-    sum = 0
-    max_count = (len(data) / 2) * 2
-    count = 0
-    while count < max_count:  # 分割数据每两比特(16bit)为一组
-        val = ord(data[count + 1]) * 256 + ord(data[count])
-        sum = sum + val
-        sum = sum & 0xffffffff
-        count = count + 2
+def get_checksum(source):
+    """
+    return the checksum of source
+    the sum of 16-bit binary one's complement
+    """
+    checksum = 0
+    count = (len(source) / 2) * 2
+    i = 0
+    while i < count:
+        temp = ord(source[i + 1]) * 256 + ord(source[i]) # 256 = 2^8
+        checksum = checksum + temp
+        checksum = checksum & 0xffffffff # 4,294,967,296 (2^32)
+        i = i + 2
 
-    if max_count < len(data):  # 如果数据长度为基数,则将最后一位单独相加
-        sum = sum + ord(data[len(data) - 1])
-        sum = sum & 0xffffffff
+    if i < len(source):
+        checksum = checksum + ord(source[len(source) - 1])
+        checksum = checksum & 0xffffffff
 
-    sum = (sum >> 16) + (sum & 0xffff)  # 将高16位与低16位相加直到高16位为0
-    sum = sum + (sum >> 16)
-    answer = ~sum
+    # 32-bit to 16-bit
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = checksum + (checksum >> 16)
+    answer = ~checksum
     answer = answer & 0xffff
+
+    # why? ans[9:16 1:8]
     answer = answer >> 8 | (answer << 8 & 0xff00)
-    return answer  # 返回的是十进制整数
+    return answer
 
 #icmp header
 class icmp_pack:
@@ -29,23 +35,25 @@ class icmp_pack:
         self.Type=0x08#request
         self.Code=0x00
         self.Checksum=0
-        self.Identifier=random.randint(1,65535)
+        self.Identifier=my_ID = os.getpid() & 0xFFFF
         self.Sequencenumber=1
     def pack(self,data_length):
-        ping_header=struct.pack('bbHH',self.Type,self.Code,self.Identifier,self.Sequencenumber)
+        ping_header=struct.pack('!bbHHh',self.Type,self.Code,self.Checksum,self.Identifier,self.Sequencenumber)
         data=(data_length-8)*'p'
-        data=struct.pack('d',time.time())+data
-        checksum=do_checksum(data)
-        ping_data=struct.pack('bbHHH',self.Type,self.Code,checksum,self.Identifier,self.Sequencenumber)
-        return ping_data
+        data=struct.pack('d',time.clock())+data
+        checksum=get_checksum(ping_header+data)
+        ping_data=struct.pack('!bbHHh',self.Type,self.Code,checksum,self.Identifier,self.Sequencenumber)
+        return ping_data+data
 
 #creat the socket
-s=socket.socket(socket.AF_INET,socket.SOCK_RAW)
+s=socket.socket(socket.AF_INET,socket.SOCK_RAW,1)
+#print socket.getprotobyname('udp')
 HOST = ''
 PORT = 0
 saddr = (HOST, PORT)
-s.bind(saddr)
+#s.bind(saddr)
 ping=icmp_pack()
-s.sendto(ping.pack(192),('114.114.114.114',0))
+s.sendto(ping.pack(192),('6.6.6.6',0))
 data,addr=s.recvfrom(1024)
-print data
+s.close()
+print data,addr
