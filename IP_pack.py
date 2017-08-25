3c27136573d81108eba8498a7fc7707e0715b8a7
兰尼特船长 <344897531@qq.com>
2017-08-25 13:05:19 +0800

    checksum
    checksum

--- IP_pack.py
+++ IP_pack.py
@@ -1,6 +1,34 @@
 #coding=utf8
 import socket,struct,time,random
 from scapy.all import *
+#校验和函数
+def get_checksum(source):
+    """
+    return the checksum of source
+    the sum of 16-bit binary one's complement
+    """
+    checksum = 0
+    count = (len(source) / 2) * 2
+    i = 0
+    while i < count:
+        temp = ord(source[i + 1]) * 256 + ord(source[i]) # 256 = 2^8
+        checksum = checksum + temp
+        checksum = checksum & 0xffffffff # 4,294,967,296 (2^32)
+        i = i + 2
+
+    if i < len(source):
+        checksum = checksum + ord(source[len(source) - 1])
+        checksum = checksum & 0xffffffff
+
+    # 32-bit to 16-bit
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = checksum + (checksum >> 16)
+    answer = ~checksum
+    answer = answer & 0xffff
+
+    # why? ans[9:16 1:8]
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
 class IP:
     def __init__(self):
         self.Version=4
